b19c4f8e452145012d677d71dc9f17a677a19c9b
Pawan Kumar <pawan.kumar5@publicissapient.com>
2022-10-21 15:54:05 +0530

    Added mock service worker

--- src/components/Dashboard/Team/index.js
+++ src/components/Dashboard/Team/index.js
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+
 export default function Team() {
-  return <div>Teams Page</div>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/users")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return (
+    <div>
+      <pre>
+        <code>{JSON.stringify(data, null, 2)}</code>
+      </pre>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import "./index.css";
 import App from "./App";
 import DashboardState from "./context/DashboardState";
 
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
